786d3e31d8d2f8bb92b9e22646a4f5cc83a6078a
Stephan Pauxberger <stephan@blackbuild.com>
2026-07-15 16:59:52 +0200

    Add real KlumAST documentation fixture

---
+++ src/test/java/org/annodoc/intellij/documentation/KlumAstQuickDocumentationTest.java
@@ -0,0 +1,82 @@
+package org.annodoc.intellij.documentation;
+
+import com.blackbuild.annodocimal.annotations.AnnoDoc;
+import com.intellij.platform.backend.documentation.DocumentationData;
+import com.intellij.platform.backend.documentation.DocumentationTarget;
+import com.intellij.platform.backend.documentation.PsiDocumentationTargetProvider;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiCompiledElement;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.PsiTestUtil;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
+import org.jetbrains.annotations.NotNull;
+
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.Objects;
+
+public final class KlumAstQuickDocumentationTest extends LightJavaCodeInsightFixtureTestCase {
+    @Override
+    protected @NotNull LightProjectDescriptor getProjectDescriptor() {
+        return JAVA_21;
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        addLibrary("real-klum-ast-fixture", Path.of(System.getProperty("annodoc.klumAstFixtureJar")));
+        addLibrary("annodocimal-annotations", libraryJarContaining(AnnoDoc.class));
+    }
+
+    public void testQuickDocumentationReadsAnnoDocFromRealKlumAstGeneratedMethodAtGroovyCallSite() {
+        PsiClass builderClass = JavaPsiFacade.getInstance(getProject()).findClass(
+                "fixture.DocumentedModel._RW",
+                GlobalSearchScope.allScope(getProject())
+        );
+        assertNotNull("The generated KlumAST builder must be available as compiled PSI", builderClass);
+
+        myFixture.configureByText(
+                "Usage.groovy",
+                """
+                import fixture.DocumentedModel._RW as DocumentedModelBuilder
+
+                DocumentedModelBuilder builder
+                builder.it<caret>em {}
+                """
+        );
+
+        PsiElement originalElement = myFixture.getFile().findElementAt(myFixture.getCaretOffset());
+        PsiElement targetElement = myFixture.getElementAtCaret();
+        assertInstanceOf(targetElement, PsiMethod.class);
+        assertInstanceOf(targetElement, PsiCompiledElement.class);
+
+        DocumentationTarget target = PsiDocumentationTargetProvider.EP_NAME.getExtensionList().stream()
+                .map(provider -> provider.documentationTarget(targetElement, originalElement))
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElse(null);
+
+        assertNotNull("The registered provider must claim the real KlumAST-generated method", target);
+        DocumentationData documentation = assertInstanceOf(target.computeDocumentation(), DocumentationData.class);
+        assertTrue(documentation.getHtml().contains("Creates a new 'item' and adds it to the 'items' collection."));
+    }
+
+    private void addLibrary(String name, Path jar) {
+        PsiTestUtil.addLibrary(getModule(), name, jar.getParent().toString(), jar.getFileName().toString());
+    }
+
+    private static Path libraryJarContaining(Class<?> type) throws Exception {
+        URL classResource = type.getResource(type.getSimpleName() + ".class");
+        if (classResource == null || !"jar".equals(classResource.getProtocol())) {
+            throw new IllegalStateException("Cannot locate test artifact containing " + type.getName());
+        }
+        String resourceUrl = classResource.toExternalForm();
+        int entrySeparator = resourceUrl.indexOf("!/");
+        return Path.of(URI.create(resourceUrl.substring("jar:".length(), entrySeparator)));
+    }
+}
